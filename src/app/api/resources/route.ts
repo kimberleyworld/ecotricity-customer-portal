9af4260a6c8e725cb209c4961ff2cfb3deb9d2f6
kimberleyworld <83449648+kimberleyworld@users.noreply.github.com>
2025-09-19 16:14:18 +0100

    create route.ts

---
+++ src/app/api/resources/route.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const res = await fetch(
+      "https://api.neso.energy/api/3/action/package_show?id=embedded-wind-and-solar-forecasts",
+      {
+        next: { revalidate: 600 }, // cache for 10 mins then refresh to keep forcast up to date
+      }
+    );
+
+    if (!res.ok) {
+      return NextResponse.json(
+        { error: "Didn't fetch resources" },
+        { status: res.status }
+      );
+    }
+
+    const data = await res.json();
+
+    return NextResponse.json(data.result.resources);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Server Error" },
+      { status: 500 }
+    );
+  }
+}
